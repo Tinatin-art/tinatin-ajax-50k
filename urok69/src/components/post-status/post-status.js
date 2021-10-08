a0c0dcb1e3118180387478a532ea691b62e0cbea
Tinatin <omurovatinatin@gmail.com>
2021-10-08 21:46:50 +0600

    urok 69 React App

---
+++ urok69/src/components/post-status/post-status.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PostStatusFilter = () => {
+    return (
+        <div className="Btn d-flex">
+            <button value="Btn1">Все</button>
+            <button value="Btn2">Понравилось</button>
+        </div>
+    )
+}
+
+
+export default PostStatusFilter
